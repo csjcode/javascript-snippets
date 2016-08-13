94bff76e31cba8c160d939aca29d13c2219508a2
csjcode <christophersstjohn@gmail.com>
2016-08-13 14:55:07 +0700

    More comments

--- Anchor-Tags/allOnClickAlertBlocked.js
+++ Anchor-Tags/allOnClickAlertBlocked.js
@@ -2,19 +2,19 @@
 var blockedDomains = new Array(
                     "washingtonpost.com",
                     "newyorktimes.com",
-                    "cnn.com");                                 //  array of blocked domains
+                    "cnn.com"
+                    );                                          //  array of blocked domains
 
 var anchors = document.getElementsByTagName("a");               //  get all anchor tags
 for (var i = 0; i < anchors.length; i++) {                      //  loop through anchor tags
     for(var k = 0; k < blockedDomains.length; k++){             //  loop through blocked domains array
         if(anchors[i].href.indexOf(blockedDomains[k]) > -1) {   //  if  href attribute and matches blocked domain
-            var anchorUrl = anchors[i].href + "\n";             //  assign var to href value
+            var anchorUrl = anchors[i].href;                    //  assign var to href value
             console.log(anchorUrl);                             //  console log href value
-        }
-    }
-
-    anchors[i].onclick = function() {                           //  on click of the anchor tags
-        alert("Blocked.\n\nDisable Blocker\n");                 //  show alert 
-        return(false);
-    };
-}
+            anchors[i].onclick = function() {                   //  if blcoked, on click of the anchor tags
+                alert("Blocked.\n\nDisable Blocker\n");         //  show alert
+                return(false);
+            };                                                  //  end on click function
+        }                                                       //  end if match on blocked domains
+    }                                                           //  end loop on blocked domains
+}                                                               //  end loop all anchor tags
