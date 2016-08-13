2e4e6110319d370cf3ba6ca870b9f643b927166a
csjcode <christophersstjohn@gmail.com>
2016-08-13 14:48:23 +0700

    Fix extra code and Add comments

--- Anchor-Tags/allOnClickAlertBlocked.js
+++ Anchor-Tags/allOnClickAlertBlocked.js
@@ -1,17 +1,20 @@
 
-var blockedDomains = new Array("washingtonpost.com","newyorktimes.com","cnn.com");
+var blockedDomains = new Array(
+                    "washingtonpost.com",
+                    "newyorktimes.com",
+                    "cnn.com");                                 //  array of blocked domains
 
-var anchors = document.getElementsByTagName("a");
-for (var i = 0; i < anchors.length; i++) {
-    for(var k = 0; k < blockedDomains.length; k++){
-        if(anchors[i].href.indexOf(blockedDomains[k]) > -1) {
-            var anchorUrl = anchors[i].href + "\n";
-            console.log(anchorUrl);
+var anchors = document.getElementsByTagName("a");               //  get all anchor tags
+for (var i = 0; i < anchors.length; i++) {                      //  loop through anchor tags
+    for(var k = 0; k < blockedDomains.length; k++){             //  loop through blocked domains array
+        if(anchors[i].href.indexOf(blockedDomains[k]) > -1) {   //  if  href attribute and matches blocked domain
+            var anchorUrl = anchors[i].href + "\n";             //  assign var to href value
+            console.log(anchorUrl);                             //  console log href value
         }
     }
 
-    anchors[i].onclick = function(anchorUrl) {
-        alert("Blocked.\n\nDisable Blocker\n" + anchorUrl);
+    anchors[i].onclick = function() {                           //  on click of the anchor tags
+        alert("Blocked.\n\nDisable Blocker\n");                 //  show alert 
         return(false);
     };
 }
