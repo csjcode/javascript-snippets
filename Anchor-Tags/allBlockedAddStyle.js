567fecaeef1cc33193abfd525f8f3e48928439e8
csjcode <christophersstjohn@gmail.com>
2016-08-13 15:24:27 +0700

    Add styles to blocked domains

--- Anchor-Tags/allBlockedAddStyle.js
+++ Anchor-Tags/allBlockedAddStyle.js
@@ -1,3 +1,4 @@
+// Loop through all anchor tags. If matching blocked domains set new style. Alert blocked.
 
 var blockedDomains = [
                     "washingtonpost.com",
@@ -10,6 +11,9 @@ for (var i = 0; i < anchors.length; i++) {                      //  loop through
     for(var k = 0; k < blockedDomains.length; k++){             //  loop through blocked domains array
         if(anchors[i].href.indexOf(blockedDomains[k]) > -1) {   //  if  href attribute and matches blocked domain
             var anchorUrl = anchors[i].href;                    //  assign var to href value
+            anchors[i].style.backgroundColor = "#6e6e6e";       //  change background color
+            anchors[i].style.color = "#6e6e6e";                 //  change text color
+            anchors[i].style.textDecoration = "none";           //  removed text decoration (href underline)
             console.log(anchorUrl);                             //  console log href value
             anchors[i].onclick = function() {                   //  if blcoked, on click of the anchor tags
                 alert("Blocked.\n\nDisable Blocker\n");         //  show alert
