7c07a7264cd2aa826f7a4e014f5cc21fe1ca9c5e
csjcode <christophersstjohn@gmail.com>
2016-08-29 23:49:38 +0700

    ES6 template literal example

---
+++ Strings/es6-template-literals.js
@@ -0,0 +1,16 @@
+
+
+// Example of basic template literal
+
+var customer = { name: "Foo" }
+var card = { amount: 7, product: "Bar", unitprice: 42 }
+var message = `Hello ${customer.name},
+want to buy ${card.amount} ${card.product} for
+a total of ${card.amount * card.unitprice} bucks?`
+
+/* output:
+"Hello Foo,
+want to buy 7 Bar for
+a total of 294 bucks?"
+
+*/
