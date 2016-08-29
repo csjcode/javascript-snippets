c3b562cfad92399b57af54cb74dba98103562dc3
csjcode <christophersstjohn@gmail.com>
2016-08-29 23:56:14 +0700

    ES6 template literal with HTML output

--- Strings/es6-template-literals.js
+++ Strings/es6-template-literals.js
@@ -16,8 +16,6 @@ today?
 
 // Example of basic template literal
 
-
-
 var customer = { name: "Foo" }
 var card = { amount: 7, product: "Bar", unitprice: 42 }
 var message = `Hello ${customer.name},
@@ -28,5 +26,38 @@ a total of ${card.amount * card.unitprice} bucks?`
 "Hello Foo,
 want to buy 7 Bar for
 a total of 294 bucks?"
+
+*/
+
+
+// Example of template literal for HTML
+
+const data = [
+    { first: '<Jane>', last: 'Bond' },
+    { first: 'Lars', last: '<Croft>' },
+];
+
+const tmpl = addrs => `
+    <table>
+    ${addrs.map(addr => `
+        <tr><td>${addr.first}</td></tr>
+        <tr><td>${addr.last}</td></tr>
+    `).join('')}
+    </table>
+`;
+console.log(tmpl(data));
+
+
+/* output
+
+    <table>
+
+        <tr><td><Jane></td></tr>
+        <tr><td>Bond</td></tr>
+
+        <tr><td>Lars</td></tr>
+        <tr><td><Croft></td></tr>
+
+    </table>
 
 */
