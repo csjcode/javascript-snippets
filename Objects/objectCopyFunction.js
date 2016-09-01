25622ee7b0aa99e351f1150601499805550cf3a3
csjcode <christophersstjohn@gmail.com>
2016-09-01 12:15:41 +0700

    Function to Copy Objects with forEach

---
+++ Objects/objectCopyFunction.js
@@ -0,0 +1,17 @@
+
+// An object copy function with forEach [src: MDN]
+
+function copy(obj) {
+  var copy = Object.create(Object.getPrototypeOf(obj));
+  var propNames = Object.getOwnPropertyNames(obj);
+
+  propNames.forEach(function(name) {
+    var desc = Object.getOwnPropertyDescriptor(obj, name);
+    Object.defineProperty(copy, name, desc);
+  });
+
+  return copy;
+}
+
+var obj1 = { a: 1, b: 2 };
+var obj2 = copy(obj1); // obj2 looks like obj1 now
