3b69ecdebe466f34cec345ee87b50292c4ff8b48
csjcode <christophersstjohn@gmail.com>
2016-09-07 01:28:13 +0700

    es6 iterators

--- Iterators/es6-iterators.js
+++ Iterators/es6-iterators.js
@@ -20,3 +20,113 @@ var it = greeting[Symbol.iterator]();
 
 it.next();      // { value: "h", done: false }
 it.next();      // { value: "e", done: false }
+
+var m = new Map();
+m.set( "foo", 42 );
+m.set( { cool: true }, "hello world" );
+
+var it1 = m[Symbol.iterator]();
+var it2 = m.entries();
+
+it1.next();     // { value: [ "foo", 42 ], done: false }
+it2.next();     // { value: [ "foo", 42 ], done: false }
+
+/* -------------------------------------------------------------------------*/
+
+// Custom fibonacci sequence (from You Don't Know JS)
+
+var Fib = {
+    [Symbol.iterator]() {
+        var n1 = 1, n2 = 1;
+
+        return {
+            // make the iterator an iterable
+            [Symbol.iterator]() { return this; },
+
+            next() {
+                var current = n2;
+                n2 = n1;
+                n1 = n1 + current;
+                return { value: current, done: false };
+            },
+
+            return(v) {
+                console.log(
+                    "Fibonacci sequence abandoned."
+                );
+                return { value: v, done: true };
+            }
+        };
+    }
+};
+
+for (var v of Fib) {
+    console.log( v );
+
+    if (v > 50) break;
+}
+// 1 1 2 3 5 8 13 21 34 55
+// Fibonacci sequence abandoned.
+
+/* -------------------------------------------------------------------------*/
+
+//    Using an Iterator with Queue actions (from You Don't Know JS)
+
+/* -------------------------------------------------------------------------*/
+
+var tasks = {
+    [Symbol.iterator]() {
+        var steps = this.actions.slice();
+
+        return {
+            // make the iterator an iterable
+            [Symbol.iterator]() { return this; },
+
+            next(...args) {
+                if (steps.length > 0) {
+                    let res = steps.shift()( ...args );
+                    return { value: res, done: false };
+                }
+                else {
+                    return { done: true }
+                }
+            },
+
+            return(v) {
+                steps.length = 0;
+                return { value: v, done: true };
+            }
+        };
+    },
+    actions: []
+};
+
+
+
+tasks.actions.push(
+    function step1(x){
+        console.log( "step 1:", x );
+        return x * 2;
+    },
+    function step2(x,y){
+        console.log( "step 2:", x, y );
+        return x + (y * 2);
+    },
+    function step3(x,y,z){
+        console.log( "step 3:", x, y, z );
+        return (x * y) + z;
+    }
+);
+
+var it = tasks[Symbol.iterator]();
+
+it.next( 10 );          // step 1: 10
+                        // { value:   20, done: false }
+
+it.next( 20, 50 );      // step 2: 20 50
+                        // { value:  120, done: false }
+
+it.next( 20, 50, 120 ); // step 3: 20 50 120
+                        // { value: 1120, done: false }
+
+it.next();              // { done: true }
