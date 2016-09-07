7ef76e5603e518ef51690e5a07a05f8ffc5e814c
csjcode <christophersstjohn@gmail.com>
2016-09-07 22:10:56 +0700

    review scope - Call and Bind snippets

---
+++ Scope/thisCallBindApply.js
@@ -0,0 +1,24 @@
+
+// call attaches this into function and executes the function immediately:
+
+var person = {
+  name: "James Smith",
+  hello: function(thing) {
+    console.log(this.name + " says hello " + thing);
+  }
+}
+
+person.hello.call(person, "world");
+
+
+// bind attaches this into function and it needs to be invoked separately like this:
+
+var person = {
+  name: "James Smith",
+  hello: function(thing) {
+    console.log(this.name + " says hello " + thing);
+  }
+}
+
+var helloFunc = person.hello.bind(person);
+helloFunc("world");  // output: "James Smith says hello world"
