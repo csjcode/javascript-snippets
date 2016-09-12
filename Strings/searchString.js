173431ab04a7ce91f93c474b284fc360da8daceb
csjcode <christophersstjohn@gmail.com>
2016-09-12 13:04:24 +0700

    Update search string

--- Strings/searchString.js
+++ Strings/searchString.js
@@ -3,7 +3,9 @@
 //1a. Basic find function in E5
 var str = "This is my string to search";
 var searchStr = "string";
-var pos = function(totalStr,stringToSearch){ return str.indexOf(stringToSearch) };
+var pos = function(totalStr,stringToSearch){
+  return str.indexOf(stringToSearch)
+};
 
 console.log(pos(str,searchStr));
 
